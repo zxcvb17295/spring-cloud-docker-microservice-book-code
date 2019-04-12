96c4c27edfb169f7e2d03bfe918dd2bf586cb9da
G-mi <zxcvb17295@gmail.com>
2019-04-13 00:48:55 +0800

    add microservice-simple-provider-user

---
+++ microservice-simple-provider-user/src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import com.example.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.repository.UserRepository;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        User findOne = this.userRepository.findOne(id);
+        return findOne;
+    }
+}
