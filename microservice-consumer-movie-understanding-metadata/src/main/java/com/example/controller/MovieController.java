c0ab646888af695d715a47d808e51e23d53b1650
G-mi <zxcvb17295@gmail.com>
2019-04-13 22:28:46 +0800

    add custom metadata

---
+++ microservice-consumer-movie-understanding-metadata/src/main/java/com/example/controller/MovieController.java
@@ -0,0 +1,32 @@
+package com.example.controller;
+
+import com.example.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+public class MovieController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+    }
+
+    @GetMapping("/user-instance")
+    public List<ServiceInstance> showInfo() {
+        return this.discoveryClient.getInstances("microservice-provider-user");
+    }
+
+}
