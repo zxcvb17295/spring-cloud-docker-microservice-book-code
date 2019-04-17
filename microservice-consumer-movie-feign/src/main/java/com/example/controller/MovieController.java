9beb80b9d7a5bfaa96e255279996de55d75df697
G-mi <zxcvb17295@gmail.com>
2019-04-18 01:09:33 +0800

    add feign

---
+++ microservice-consumer-movie-feign/src/main/java/com/example/controller/MovieController.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import com.example.entity.User;
+import com.example.feign.UserFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MovieController {
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.userFeignClient.findById(id);
+    }
+}
